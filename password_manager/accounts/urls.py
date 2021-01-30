ef03b12b350f8990f494608ebfcfa8ea0015bd16
Oren Rimer <76216721+orenrimer@users.noreply.github.com>
2021-01-30 22:55:42 +0200

    Update urls.py

--- password_manager/accounts/urls.py
+++ password_manager/accounts/urls.py
@@ -5,6 +5,7 @@ from .views import (
     logout_view,
     account_page_view,
     must_authenticate_view,
+    unauthorized_view,
 )
 
 app_name = "accounts"
@@ -15,5 +16,5 @@ urlpatterns = [
     path('my-account/', account_page_view, name="account"),
     path('logout/', logout_view, name="logout"),
     path('must-authenticate/',must_authenticate_view, name="must_authenticate"),
+    path('unauthorized/',unauthorized_view, name="unauthorized"),
 ]
-
